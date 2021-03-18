0cc808b57e8262d8c632f2ecf00d6550cf431a47
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2021-03-18 17:25:14 -0400

    Deleted unnecessary file

--- webformmailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $to = "fordhamramshockey@gmail.com"; // <– replace with your address here
-   $subject = "Test mail";
-   $message = "Hello! This is a simple test email message.";
-   $from = "fordhamramshockey@gmail.com";
-   $headers = "From:" . $from;
-   mail($to,$subject,$message,$headers);
-   echo "Mail Sent.";
-?>
