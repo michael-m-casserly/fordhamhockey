a8b0c95640c43a3634c0c6f970995b6a2f7bc03a
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2021-03-18 16:11:58 -0400

    Updated all references to @fordhamramshockey.com

--- webformmailer.php
+++ webformmailer.php
@@ -1,8 +1,8 @@
 <?php
-   $to = "web-form-submission@fordhamramshockey.com"; // <– replace with your address here
+   $to = "fordhamramshockey@gmail.com"; // <– replace with your address here
    $subject = "Test mail";
    $message = "Hello! This is a simple test email message.";
-   $from = "mcasserly@fordhamramshockey.com";
+   $from = "fordhamramshockey@gmail.com";
    $headers = "From:" . $from;
    mail($to,$subject,$message,$headers);
    echo "Mail Sent.";
