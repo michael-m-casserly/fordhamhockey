86fc781dabefffde0bc79c41f728642d3235b2de
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2021-03-18 18:27:49 -0400

    Moved Recruit Form to Google Forms

---
+++ web-form-builder/fordhamhockey-contact-us_exported/fordhamhockey-contact-us_exported/fordhamhockey-contact-us/validation_data.js
@@ -0,0 +1,3 @@
+ var data_validation = '{"fb-submit-button":{"hover":{"background-image":""}},"item1_text_1":{"required":true},"item3_text_1":{"required":true},"item5_email_1":{"email":true,"required":true},"item7_0_radio":{"required":false},"item8_0_radio":{"required":false},"item9_textarea_1":{"maxlength":"10000"},"reCaptcha":{"captcha":true}}';
+ var data_jsplugins = '[]';
+ var data_cssplugins = '[]';
