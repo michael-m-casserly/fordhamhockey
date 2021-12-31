4a809cf9c7239f8881633e35639a625518c79b69
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2021-12-31 16:04:29 -0500

    FCW-19

--- form-tools/global/config.php
+++ form-tools/global/config.php
@@ -1,14 +1,27 @@
-<?php
-
-// main program paths - no trailing slashes!
-$g_root_url = 'http://www.fordhamramshockey.com/form-tools';
-$g_root_dir = '/home/mmcfuhockey/public_html/form-tools';
-
-// database settings
-$g_db_hostname = 'localhost';
-$g_db_name = 'i3320908_ft1';
-$g_db_username = 'i3320908_ft1';
-$g_db_password = 'P.rsO^5QsDfZKFIcN1.09[[9';
-$g_table_prefix = 'ft_';
-
+<?php
+
+
+www.fordhamhockey.com
+// main program paths - no trailing slashes!
+
+$g_root_url = 'http://www.fordhamramshockey.com/form-tools';
+
+$g_root_dir = '/home/mmcfuhockey/public_html/form-tools';
+
+
+
+// database settings
+
+$g_db_hostname = 'localhost';
+
+$g_db_name = 'i3320908_ft1';
+
+$g_db_username = 'i3320908_ft1';
+
+$g_db_password = 'P.rsO^5QsDfZKFIcN1.09[[9';
+
+$g_table_prefix = 'ft_';
+
+
+
 ?>
--- javascript/printer-friendly.js
+++ javascript/printer-friendly.js
@@ -18,9 +18,9 @@ function PrintThisPage()
 
        winprint.document.open(); 
 
-       winprint.document.write('<html><head><title>Fordham Hockey</title><link rel=stylesheet type=text/CSS href="http://www.fordhamramshockey.com/fordhamhockey.css" /></head>');
+       winprint.document.write('<html><head><title>Fordham Hockey</title><link rel=stylesheet type=text/CSS href="http://www.fordhamhockey.com/fordhamhockey.css" /></head>');
 
-       winprint.document.write('<body><table width="100%"><tr><td align="left">&nbsp;</td><td align="center"></td><td align="right"><a class="textlink" href="javascript:print();">Print</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a class="textlink" href="javascript:close();">Close Window</a></td></tr><tr><td colspan="3"><hr align="right"></td></tr><tr><td align="left" width="33%"><img src="http://www.fordhamramshockey.com/images/logo_fordhamhockey.jpg"></td><td align="center" width="33%"><img src="http://www.fordhamramshockey.com/images/FordhamHockeyBanner.gif"></td><td align="right" width="33%"><img src="http://www.fordhamramshockey.com/images/logo_fordhamhockey.jpg"></td></tr></table><br>');
+       winprint.document.write('<body><table width="100%"><tr><td align="left">&nbsp;</td><td align="center"></td><td align="right"><a class="textlink" href="javascript:print();">Print</a>&nbsp;&nbsp;-&nbsp;&nbsp;<a class="textlink" href="javascript:close();">Close Window</a></td></tr><tr><td colspan="3"><hr align="right"></td></tr><tr><td align="left" width="33%"><img src="http://www.fordhamhockey.com/images/logo_fordhamhockey.jpg"></td><td align="center" width="33%"><img src="http://www.fordhamhockey.com/images/FordhamHockeyBanner.gif"></td><td align="right" width="33%"><img src="http://www.fordhamhockey.com/images/logo_fordhamhockey.jpg"></td></tr></table><br>');
 
        winprint.document.write(sWinHTML);          
 
